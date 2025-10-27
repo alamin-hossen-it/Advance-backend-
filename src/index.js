5181a1d0c40d44787c2e1407ff180bb0c6383517
alamin-hossen-it <alamin.it24to7@.com>
2025-10-27 18:07:17 +0300

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 import dataBaseConnect from "./db/database.js";
-dotenv.config({
-    path: "./.env"
+import app from "./app.js";
+
+
+dotenv.config()
+dataBaseConnect().then(()=>{
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`Server is running at http://localhost:${process.env.PORT}`)
+    })
+}).catch((err)=>{ 
+    console.log("MongoDB Connection is faield!1",err)
 })
 
-
-dataBaseConnect()
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+  process.exit(1);
+});
